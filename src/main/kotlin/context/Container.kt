decebacc4c8d8d48a961bfa3adf8ce8ab24aba46
aryumka <yooooar@gmail.com>
2024-01-20 18:28:43 +0900

    remove unnecessary files

--- src/main/kotlin/context/Container.kt
+++ src/main/kotlin/context/Container.kt
@@ -7,8 +7,21 @@ class Container {
     private val registeredBean = mutableMapOf<String, Any>()
 
     fun register(name: String, kClass: KClass<*>) {
-        val bean = kClass
-        registeredBean[name] = bean
+        registeredBean[name] =
+    }
+
+    fun createBean(kClass: KClass<*>): Any {
+        val constructor = kClass.constructors.first()
+        val parameters = constructor.parameters
+        val args = parameters.map { parameter ->
+            val bean = registeredBean[parameter.name]
+            if (bean == null) {
+                createBean(parameter.type.classifier as KClass<*>)
+            } else {
+                bean
+            }
+        }
+        return constructor.call(*args.toTypedArray())
     }
 
     fun <T> getBean(name: String): T {
